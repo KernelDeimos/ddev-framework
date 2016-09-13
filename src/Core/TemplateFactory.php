68ab4cfbb4b0b89c0d97d369206ba45d154e3648
Kernel Deimos <eric.alex.dube@gmail.com>
2016-09-13 00:54:14 -0400

    Add variables to template factory

--- src/Core/TemplateFactory.php
+++ src/Core/TemplateFactory.php
@@ -2,10 +2,40 @@
 
 namespace DDev\Core;
 
-class TemplateFactory {
+class TemplateFactor implements ArrayAccess {
+	function __construct() {
+		$this->props = array();
+	}
+	function offsetSet($key, $value) {
+		if (is_null($key)) {
+			// Do nothing - property values should not
+			// be appended with an arbitrary key
+			return;
+		}
+		$this->props[$key] = $value;
+	}
+	function offsetExists($key) {
+		return isset($this->props[$key]);
+	}
+	function offsetUnset($key) {
+		unset($this->props[$key]);
+	}
+	function offsetGet($key) {
+		return $this->props[$key];
+	}
+
+	function set_webpath($value) {
+		$this->webpath = $value;
+	}
 	function make_tmpl($filepath) {
 		$tmpl = new Template();
 		$tmpl->set_template_file($filepath);
+
+		// Add template properties
+		foreach ($this->props as $key => $value) {
+			$tmpl->$key = $value;
+		}
+		
 		return $tmpl;
 	}
 }
